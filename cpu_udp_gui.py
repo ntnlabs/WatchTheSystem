0f746efd762ea1978909aa534fc44cba3b6b6a1e
NTN Labs <ntn.labs@gmail.com>
2021-01-25 09:38:07 +0100

    Update cpu_udp_gui.py
    Added comments

--- cpu_udp_gui.py
+++ cpu_udp_gui.py
@@ -1,31 +1,38 @@
+# lets import some stuff :)
 import psutil, socket, guizero
 from time import sleep
 
-msg = ""
-serverAddressPort = ("rainbow", 12345)
-bufferSize = 32
-Run = False
+# define variables
+msg = ""                                  # the message that will be sent
+serverAddressPort = ("rainbow", 12345)    # server hostname/ip and port
+bufferSize = 32                           # buffer size
+Run = False                               # just in case we wanna control the 
 
+# let's start the sending
 def Start_meranie():
   global Run
   Run = True
   stop.enable()
   start.disable()
 
+# do not send any data anymore
 def Stop_meranie():
   global Run
   Run = False
   stop.disable()
   start.enable()
 
+# most important function
 def Ukonci_meranie():
   exit()
 
+# collect data pack it and send it away
 def Read_Send():
   if Run:
     msg = "cpu:"+str(int(psutil.cpu_percent()))
     UDPClientSocket.sendto(msg.encode(), serverAddressPort)
 
+# let's build the gui
 aplikacia = guizero.App(title="Meranie CPU na dialku")
 okienko = guizero.Box(aplikacia, layout = "grid")
 start = guizero.PushButton(okienko, command=Start_meranie, text = "Start", grid = [0,0])
@@ -33,6 +40,7 @@ stop = guizero.PushButton(okienko, command=Stop_meranie, text = "Stop", grid = [
 koniec = guizero.PushButton(okienko, command=Ukonci_meranie, text = "Koniec", grid = [2,0])
 stop.disable()
 
+# main loop
 try:
   UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
   aplikacia.repeat(400, Read_Send)
